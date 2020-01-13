a0b4706751c950db0eb5e54d788fc74da8e6c987
Sokawaii <52569738+Sokawaii25@users.noreply.github.com>
2020-01-13 09:17:25 +0100

    début tri par contenu

--- sujet.sh
+++ sujet.sh
@@ -9,31 +9,32 @@ parcours_dossiers()
     do
 	    liste[${#liste[*]}]=$i
     done
-    test=`pwd`
+    chemin=`pwd`
     for dossier in ${liste[@]}
     do
-	cd $test/$dossier
+	cd $chemin/$dossier
 	parcours_dico
-	echo ${liste[@]}
     done
+    cd $chemin
+    tri_contenu
 }
 
 #le dictionnaire est composé de lignes, chaque ligne ressemble à ça : nom_dossier,extension1,extension2...
 
 parcours_dico(){            #cette fonction classifie le dossier pour chaque ligne
-	lignes=`wc -l ../classement.txt | sed -e "s| ../classement.txt||g"` #on récupère le nombre de lignes 
+	lignes=`wc -l ../dico_triExtension.txt | sed -e "s| ../dico_triExtension.txt||g"` #on récupère le nombre de lignes 
 	for i in `seq 1 $lignes`    #et on classe (fonction classify) pour chaque ligne du dico
  	do
-       	    ligne=`cat ../classement.txt | awk -v i=$i 'NR == i {print;}'`
+       	    ligne=`cat ../dico_triExtension.txt | awk -v i=$i 'NR == i {print;}'`
        	    listeExt=()
        	    for mot in $ligne; do
 		listeExt[${#listeExt[*]}]=$mot
        	    done
-       	    classify_cat
+       	    tri_extension
 	done
 }
 
-classify_cat()
+tri_extension()
 {
 	for file in * 
 	do 
@@ -46,6 +47,23 @@ classify_cat()
                 	mv $file ./${listeExt[0]}/
         	fi
     	done
+}
+
+function tri_contenu()
+{
+    if [ -d Documents ]; then
+	cd $chemin/Documents
+	lignes2=`wc -l ../dico_triContenu.txt | sed -e "s| ../dico_triContenu.txt||g"` #on récupère le nombre de lignes 
+	for i in `seq 1 $lignes2`    #et on classe (fonction classify) pour chaque ligne du dico
+ 	do
+       	    ligne2=`cat ../dico_triContenu.txt | awk -v i=$i 'NR == i {print;}'`
+       	    listeMots=()
+       	    for mot in $ligne2; do
+		listeMots[${#listeMots[*]}]=$mot
+       	    done
+       	    
+	done
+    fi
 }
 
 parcours_dossiers
