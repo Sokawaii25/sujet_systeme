edec20908e2cfc1ba4890e2e6a314c80f843cb0e
Sokawaii <52569738+Sokawaii25@users.noreply.github.com>
2020-01-16 14:10:23 +0100

    maj tri par contenu
    tout marche à part la récursivité de l'autre péruvien

--- sujet.sh
+++ sujet.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
-liste=()
  
-parcours_dossiers()
+function parcours_dossiers()
 {
+    liste=()
     dossiers=`ls -d */`
     for i in $dossiers
     do
-	    liste[${#liste[*]}]=$i
+	liste[${#liste[*]}]=$i
     done
     chemin=`pwd`
     for dossier in ${liste[@]}
@@ -15,69 +14,64 @@ parcours_dossiers()
 	cd $chemin/$dossier
 	parcours_dico
     done
-    cd $chemin
-    tri_contenu
 }
 
 #le dictionnaire est composé de lignes, chaque ligne ressemble à ça : nom_dossier,extension1,extension2...
 
-parcours_dico(){            #cette fonction classifie le dossier pour chaque ligne
-	lignes=`wc -l ../dico_triExtension.txt | sed -e "s| ../dico_triExtension.txt||g"` #on récupère le nombre de lignes 
-	for i in `seq 1 $lignes`    #et on classe (fonction classify) pour chaque ligne du dico
- 	do
-       	    ligne=`cat ../dico_triExtension.txt | awk -v i=$i 'NR == i {print;}'`
-       	    listeExt=()
-       	    for mot in $ligne; do
-		listeExt[${#listeExt[*]}]=$mot
-       	    done
-       	    tri_extension
-	done
+function parcours_dico(){            #cette fonction classifie le dossier pour chaque ligne
+    lignes=`wc -l < $chemin/dico_triExtension.txt` #on récupère le nombre de lignes 
+    for i in `seq 1 $lignes`    #et on classe (fonction classify) pour chaque ligne du dico
+    do
+       	ligne=`cat $chemin/dico_triExtension.txt | awk -v i=$i 'NR == i {print;}'`
+       	listeExt=()
+       	for mot in $ligne; do
+	    listeExt[${#listeExt[*]}]=$mot
+       	done
+       	tri_extension
+    done
 }
 
 tri_extension()
 {
-	for file in * 
-	do 
-		#tester chaque extension de la catégorie sur la commande `ls | grep -e extension`
-		extension=`echo $file | sed 's/.*\.//g'`			
-	        if [[ " ${listeExt[@]} " =~ " ${extension} " ]]; then #array sera la liste des extensions courantes
-                	if [ ! -d ./${listeExt[0]} ]; then
-                		mkdir ./${listeExt[0]}
-            		fi
-                	mv $file ./${listeExt[0]}/
+    for file in * 
+    do 
+	    #tester chaque extension de la catégorie sur la commande `ls | grep -e extension`
+	    extension=`echo $file | sed 's/.*\.//g'`			
+	    if [[ " ${listeExt[@]} " =~ " ${extension} " ]]; then
+                    if [ ! -d ./${listeExt[0]} ]; then
+                	mkdir ./${listeExt[0]}
+            	    fi
+                    mv $file ./${listeExt[0]}/
         	fi
-    	done
+    done
+    if [ ${listeExt[0]} = "Documents" ]; then
+	cd ${listeExt[0]}
+	tri_contenu
+    fi
 }
 
 function tri_contenu()
 {
-    if [ -d Documents ]; then
-	cd $chemin/Documents
-	lignes2=`wc -l ../dico_triContenu.txt | sed -e "s| ../dico_triContenu.txt||g"` #on récupère le nombre de lignes 
-	for i in `seq 1 $lignes2`    #et on classe (fonction classify) pour chaque ligne du dico
- 	do
-       	    ligne2=`cat ../dico_triContenu.txt | awk -v i=$i 'NR == i {print;}'`
-       	    listeMots=()
-       	    for mot in $ligne2; do
-		listeMots[${#listeMots[*]}]=$mot
-       	    done
-       	    #partie tri par contenu
-	    for file2 in *; do
-	    	extension2=`echo $file | sed 's/.*\.//g'`
-	    	if [ $extension2="txt" ]; then
-	    		texte=`cat $file`
-			for mot in texte; do
-				if [[ " ${listeMots[@]} " =~ " ${mot} " ]]; then #array sera la liste des extensions courantes
-                			if [ ! -d ./${listeMots[0]} ]; then
-                				mkdir ./${listeMots[0]}
-            				fi
-                			mv $file ./${listeMots[0]}/
-        			fi
-			done
-		fi
+    nblignes=`wc -l < $chemin/dico_triContenu.txt` #on récupère le nombre de lignes 
+    for i in `seq 1 $nblignes`    #et on classe pour chaque ligne du dico
+    do
+	ligne2=`cat $chemin/dico_triContenu.txt | awk -v i=$i 'NR == i {print;}'`
+	folder=`echo $ligne2 | cut -d\  -f1`
+	for mot in $ligne2; do
+	    #partie tri par contenu
+	    occurences=`grep -is $mot *`
+	    if [ "$occurences" != "" ]; then 
+		filenames=`echo $occurences | cut -d: -f1`
+		extension2=`echo $filename | sed 's/.*\.//g'`
+		if [ "$extension2" = "txt" ]; then
+		    if [ ! -d ./$folder ]; then
+               		mkdir ./$folder
+          	    fi
+		    mv $filenames ./$folder/
+ 		fi
+	    fi
 	done
-    fi
+    done
 }
 
 parcours_dossiers
-# on peut faire sh s.sh param
